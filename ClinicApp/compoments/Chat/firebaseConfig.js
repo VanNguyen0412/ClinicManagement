cd8d4ebce4f57633caf4fbae48428d86b5fb1164
VanNguyenIT <109479069+VanNguyen0412@users.noreply.github.com>
2024-10-16 16:38:21 +0700

    Update firebaseConfig.js

--- ClinicApp/compoments/Chat/firebaseConfig.js
+++ ClinicApp/compoments/Chat/firebaseConfig.js
@@ -10,14 +10,14 @@ import "firebase/functions";
 import "firebase/storage";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB6bDvoYmzRiOIGGtLkZut0aHVeJ8nUB5c",
-    authDomain: "chat-bf93c.firebaseapp.com",
-    databaseURL: "https://chat-bf93c-default-rtdb.firebaseio.com",
-    projectId: "chat-bf93c",
-    storageBucket: "chat-bf93c.appspot.com",
-    messagingSenderId: "518943141537",
-    appId: "1:518943141537:web:edc1278eb2cca93b8c8ae0",
-    measurementId: "G-SST024F754"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
 
 // Khởi tạo Firebase
